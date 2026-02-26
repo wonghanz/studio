429bc145ef4953a9119d2e1784bd37874cee6ddc
Wong Hanz <wonghanz2007@gmail.com>
2026-02-26 17:56:42 +0000

    chaneg the genkit.ts to this import {genkit} from 'genkit'; import {goog

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,7 @@
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),
-  ],
+  plugins: [googleAI({apiKey: "AIzaSyAGvrPIK940-JoaG6XvZy5XYHe-XJ2qKNA"})],
   model: 'googleai/gemini-2.5-pro',
 });
